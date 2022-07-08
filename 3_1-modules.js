6b65e479469d37314aa6e78610ccda7ff0e16150
Administrator Alfredo Rodriguez <alfrodria@ttigroup.mx>
2022-07-08 16:15:58 -0500

    first commit

---
+++ 3_1-modules.js
@@ -0,0 +1,12 @@
+// Modules. // those are some different files
+
+const john = 'john'
+const peter = 'peter'
+
+const sayHi = (name) =>{
+    console.log(`Hello there ${name}`);
+}
+
+sayHi('susan')
+sayHi(john)
+sayHi(peter)
